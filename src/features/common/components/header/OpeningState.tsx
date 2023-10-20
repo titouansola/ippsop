c5879213f40191f0ddc3d64c162a476a83f4abb1
Titouan Sola <titouansola0@gmail.com>
2023-10-20 10:05:07 +0200

    test log time - 2

--- src/features/common/components/header/OpeningState.tsx
+++ src/features/common/components/header/OpeningState.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import { getClient } from '@ippsop/lib/sanity.client';
 import { Schedule, Timing } from '@ippsop/lib/models/schedule';
 import { weekdays } from '@ippsop/lib/models/weekdays';
+import { logger } from '@ippsop/lib/logger';
 
 function convertHoursToNumber(timing: Timing) {
   const [hours, minutes] = timing.split('h');
@@ -12,7 +13,6 @@ export async function OpeningState() {
   const client = getClient();
   const schedules = await client.fetch<Schedule[]>("*[_type == 'schedule']");
   const date = new Date();
-  console.log(date.toUTCString(), date.getTimezoneOffset());
   const today = weekdays[date.getDay() - 1];
   const currentTiming = date.getHours() + date.getMinutes() / 60;
   const daySchedule = schedules.find(({ day }) => day === today);
@@ -22,6 +22,9 @@ export async function OpeningState() {
   const opensAt = convertHoursToNumber(daySchedule.opensAt);
   const closesAt = convertHoursToNumber(daySchedule.closesAt);
   const opened = opensAt < currentTiming && currentTiming < closesAt;
+
+  logger.info(date.toUTCString());
+  logger.info(date.getHours());
 
   return (
     <div className={'hidden flex-col items-end gap-3 lg:flex'}>
